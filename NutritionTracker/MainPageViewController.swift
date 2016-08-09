1ac2cba591cbf8d4f46ab725acaffb16aa13bf94
gabriel0746 <gabriel-allen@hotmail.com>
2016-08-09 16:36:36 -0700

    Completed UI Views

---
+++ NutritionTracker/MainPageViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MainPageViewController.swift
+//  NutritionTracker
+//
+//  Created by Gabriel Allen on 8/5/16.
+//  Copyright © 2016 Gabriel Allen. All rights reserved.
+//
+
+import Foundation
---
+++ NutritionTracker/secondPageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  secondPageViewController.swift
+//  NutritionTracker
+//
+//  Created by Gabriel Allen on 8/9/16.
+//  Copyright © 2016 Gabriel Allen. All rights reserved.
+//
+
+import UIKit
+
+class secondPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
