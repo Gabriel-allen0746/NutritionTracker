8faec857ec1e20506b2220d28778f5426fddb3ef
gabriel0746 <gabriel-allen@hotmail.com>
2016-08-10 11:11:44 -0700

    Added Date

--- NutritionTracker/MainPageViewController.swift
+++ NutritionTracker/MainPageViewController.swift
@@ -2,8 +2,34 @@
 //  MainPageViewController.swift
 //  NutritionTracker
 //
-//  Created by Gabriel Allen on 8/5/16.
+//  Created by Gabriel Allen on 8/9/16.
 //  Copyright © 2016 Gabriel Allen. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class MainPageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
